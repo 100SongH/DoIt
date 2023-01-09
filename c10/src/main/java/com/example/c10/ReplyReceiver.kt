f3f7a0ffc1ac5fa2a99aeb4ba292f6905a035bf2
100SongH <thdgml322@seoultech.ac.kr>
2023-01-09 15:53:35 +0900

    10-6 카카오톡 알림 만들기

---
+++ c10/src/main/java/com/example/c10/ReplyReceiver.kt
@@ -0,0 +1,25 @@
+package com.example.c10
+
+import android.app.NotificationManager
+import android.app.RemoteInput
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class ReplyReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+
+        // 알림의 입력 글 획득
+        val replyText = RemoteInput.getResultsFromIntent(intent)
+            ?.getCharSequence("key_text_reply")
+        Log.d("chk", "replyTxt : $replyText")
+
+        // 알림 취소
+        val manager = context.getSystemService(
+            AppCompatActivity.NOTIFICATION_SERVICE) as NotificationManager
+        manager.cancel(11)
+    }
+}
